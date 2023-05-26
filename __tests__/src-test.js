af691a411b0b4a94568337e13f729b290a711e89
kasturada1 <kazuki1.katsurada@gmail.com>
2023-05-26 16:10:22 +0900

    Add createTextElement function and its test

--- __tests__/src-test.js
+++ __tests__/src-test.js
@@ -1,11 +1,26 @@
-import { createElement } from '../src';
+import { createElement, createTextElement } from '../src';
 
 test('createElement', () => {
-  const element = createElement('div', {});
+  let element = createElement('div', {});
   expect(element).toEqual({
     type: 'div',
     props: {
       children: [],
     },
   });
+});
+
+test('createTextElement', () => {
+  let text = 'Hello, world!';
+  let expected = {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+
+  let result = createTextElement(text);
+
+  expect(result).toEqual(expected);
 });
--- src.js
+++ src.js
@@ -11,3 +11,13 @@ export function createElement(type, props, ...children) {
     },
   };
 }
+
+export function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
